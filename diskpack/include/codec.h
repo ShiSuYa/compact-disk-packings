11dd8ff2427933ee86a3f29ac63fae5eaba136aa
ShiSuYa <59110564+ShiSuYa@users.noreply.github.com>
2025-09-21 20:29:34 +0300

    Update codec.h

--- diskpack/include/codec.h
+++ diskpack/include/codec.h
@@ -9,12 +9,10 @@
 
 namespace diskpack {
 
-// SVG экспорт упаковки дисков
 void exportToSVG(const std::string& filename, 
                 const std::list<Disk>& disks,
                 double boundaryRadius);
 
-// Кодирование/декодирование областей
 struct RegionData {
     std::vector<Point> centers;
     std::vector<Interval> radii;
@@ -24,6 +22,7 @@ std::string encodeRegions(const std::vector<DiskRegion>& regions);
 bool decodeRegions(const std::string& json, RegionData& output);
 bool decodeRegions(std::istream& stream, RegionData& output);
 
-} // namespace diskpack
+}
+
 
 #endif // DISKPACK_CODEC_H
